1a68941597b5f9dda5bbc28441860775aab7fd8f
Jelle van den Broek <jelle.vandenbroek@student.hu.nl>
2020-03-27 15:14:09 +0100

    fixed up formula

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -9,9 +9,9 @@ IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &RGB_
     intensity_image->set(RGB_image.getWidth(), RGB_image.getHeight());
     int nPixels = RGB_image.getWidth() * RGB_image.getHeight();
     for (int i = 0; i < nPixels; i++) {
-        intensity_image->setPixel(i, (RGB_image.getPixel(i).r + RGB_image.getPixel(i).g + RGB_image.getPixel(i).b) / 3); // Intensity
+        //intensity_image->setPixel(i, (RGB_image.getPixel(i).r + RGB_image.getPixel(i).g + RGB_image.getPixel(i).b) / 3); // Intensity
         //intensity_image->setPixel(i, (0.3 * RGB_image.getPixel(i).r) + (0.59 * RGB_image.getPixel(i).g) + (0.11 * RGB_image.getPixel(i).b)); // Luminance
-        //intensity_image->setPixel(i, pow((0.2126 * RGB_image.getPixel(i).r), 1 / 2.2), pow((0.7152 * RGB_image.getPixel(i).g), 1 / 2.2), pow((0.0722 * RGB_image.getPixel(i).b), 1 / 2.2)); // Luma
+        intensity_image->setPixel(i, (0.2126 * pow(RGB_image.getPixel(i).r, 1 / 2.2)) + (0.7152 * pow(RGB_image.getPixel(i).g, 1 / 2.2)) + (0.0722 * pow(RGB_image.getPixel(i).b, 1 / 2.2))); // Luma
     }
     return intensity_image;
 }
