9f877001f4eb4196bd152fd27c860453acc8b3e1
Jelle van den Broek <jelle.vandenbroek@student.hu.nl>
2020-03-27 15:10:03 +0100

    Added quickly made luma formula with gamma correction

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,5 +1,6 @@
 #include "StudentPreProcessing.h"
 #include "ImageFactory.h"
+#include <math.h>
 
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &RGB_image) const {
 	//return nullptr;
@@ -9,7 +10,8 @@ IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &RGB_
     int nPixels = RGB_image.getWidth() * RGB_image.getHeight();
     for (int i = 0; i < nPixels; i++) {
         intensity_image->setPixel(i, (RGB_image.getPixel(i).r + RGB_image.getPixel(i).g + RGB_image.getPixel(i).b) / 3); // Intensity
-        intensity_image->setPixel(i, (RGB_image.getPixel(i).r * 0.3) + (RGB_image.getPixel(i).g * 0.59) + (RGB_image.getPixel(i).b * 0.11)); // Luminance
+        //intensity_image->setPixel(i, (0.3 * RGB_image.getPixel(i).r) + (0.59 * RGB_image.getPixel(i).g) + (0.11 * RGB_image.getPixel(i).b)); // Luminance
+        //intensity_image->setPixel(i, pow((0.2126 * RGB_image.getPixel(i).r), 1 / 2.2), pow((0.7152 * RGB_image.getPixel(i).g), 1 / 2.2), pow((0.0722 * RGB_image.getPixel(i).b), 1 / 2.2)); // Luma
     }
     return intensity_image;
 }
